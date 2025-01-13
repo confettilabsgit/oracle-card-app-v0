26e4c96ff5bd5e846c96fb263149386a613b3ca8
Amir <microamir@gmail.com>
2025-01-13 13:00:28 -0800

    Update next.config.js for Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Add any necessary configuration here
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+  
+  module.exports = nextConfig
+  
+  
